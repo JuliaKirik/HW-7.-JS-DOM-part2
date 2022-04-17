b63d8e209d61977ba5076e3bc28b4b31f5ad7047
JuliaKirik <juliakirichok9878@gmail.com>
2022-04-17 19:18:06 +0300

    Updated

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ const sel = document.querySelector('#select');
 let div = document.querySelector('#figure');
 sel.addEventListener('change', () => {
     let value = sel.value;
-    div.removeAttribute('class')
     div.setAttribute('class', value)
 });
 
 
 const col = document.querySelector('#color');
-const but = document.querySelector('#but')
+const but = document.querySelector('#changeColor');
 but.addEventListener('click', () => {
     let value = col.value;
     div.style.background = value;
